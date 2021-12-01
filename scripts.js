d69146457d2e337f447272fb23466b4d9b785b57
Brian Currie <brian.currie@hotmail.com>
2021-12-01 15:56:47 -0800

    Toggle dark overlay for mobile nav

--- scripts.js
+++ scripts.js
@@ -6,9 +6,20 @@ const nav_closed = document.getElementsByClassName('navbar_mobile_closed')[0];
 nav_openBtn.addEventListener('click', () => {
     nav_closed.style.display = 'none';
     nav_opened.style.display = 'flex';
+    toggleDark();
 });
 
 nav_closeBtn.addEventListener('click', () => {
     nav_closed.style.display = 'flex';
     nav_opened.style.display = 'none';
+    toggleDark();
 });
+
+function toggleDark() {
+    const overlay = document.getElementsByClassName('darken_overlay')[0];
+    if (overlay.style.display === 'block') {
+        overlay.style.display = 'none';
+    } else {
+        overlay.style.display = 'block';
+    }
+}
